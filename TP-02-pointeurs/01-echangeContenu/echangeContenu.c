8fad20c69b3494c39fe95a75dd44d83544c8e03d
Votre Nom <Vous@exemple.com>
2022-10-11 17:47:51 +0200

    tp2

--- TP-02-pointeurs/01-echangeContenu/echangeContenu.c
+++ TP-02-pointeurs/01-echangeContenu/echangeContenu.c
@@ -1,3 +1,17 @@
 #include "echangeContenu.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-// TODO implementer echangeContenu
+void echangeContenu(int *a, int *b){
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int main(void){
+    int a = 1;
+    int b = 2;
+    echangeContenu(&a,&b);
+    printf("%d",a);
+    return 0;
+}
